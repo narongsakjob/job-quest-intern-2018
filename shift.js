be51a811521692bbc9decf08d88a13eeb0cae52a
Narongsak <narongsak.chobsri@gmail.com>
2018-02-02 03:50:29 +0700

    Add shift right function and Reflactor code

--- shift.js
+++ shift.js
@@ -1,10 +1,12 @@
-const shift = (arr, type, amount) => {
-	if( type === 'left') {
-		arr = arr.concat(arr.splice(0,amount))
-	}else {
+const shift = (arr, type, amount) => (
+	arr = (type === 'left' ?  left(arr, amount) : right(arr, amount))
+)
+const left = (arr, amount) => (
+	arr.concat(arr.splice(0,amount))
+)
+const right = (arr, amount) => (
+	arr.concat(arr.splice(0, arr.length - amount))
+)
 
-	}
-	console.log(arr)
-}
-
-shift( ['john', 'jane', 'sarah', 'alex'], "left", 2 )
+console.log(shift( ['john', 'jane', 'sarah', 'alex'], "left", 2 ) )
+console.log(shift( [1, 2, 3, 4 ,5], 'right', 3 ))
